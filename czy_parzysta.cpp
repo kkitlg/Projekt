5277a51bd9bb2871edf2bbc68566d98a8f020497
KK <kkitlg@gmail.com>
2023-03-21 18:14:21 +0100

    Dodanie testow

--- czy_parzysta.cpp
+++ czy_parzysta.cpp
@@ -10,6 +10,20 @@ bool czy_parzysta (int liczba_calkowita)
         return false;
 }
 
+bool test_czy_parzysta (int testowana_liczba, bool oczekiwany_wynik)
+{
+    bool wynik;
+    wynik = czy_parzysta (testowana_liczba);
+    if (wynik == oczekiwany_wynik)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
     int liczba_calkowita = 5;
@@ -18,8 +32,34 @@ int main()
     wynik = czy_parzysta (liczba_calkowita);
     if (wynik == 0)
     {
-        cout << "Nie" << endl;
+        cout << "Nie" << endl << endl;
     }
-    else cout << "Tak" << endl;
+    else
+    {
+        cout << "Tak" << endl << endl;
+    }
+
+    bool wynik_testu;
+    cout << "Uruchomienie testu dla 1:" << endl << endl;
+    wynik_testu = test_czy_parzysta (1, false);
+    if (wynik_testu == 0)
+    {
+        cout << "Test sie nie powiodl" << endl << endl;
+    }
+    else
+    {
+        cout << "Test sie powiodl" << endl << endl;
+    }
+    cout << "Uruchomienie testu dla 2:" << endl << endl;
+    wynik_testu = test_czy_parzysta (2, true);
+    if (wynik_testu == 0)
+    {
+        cout << "Test sie nie powiodl" << endl << endl;
+    }
+    else
+    {
+        cout << "Test sie powiodl" << endl << endl;
+    }
+
     return 0;
 }
